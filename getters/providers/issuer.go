d6ca68d341d360ad648433015124cd2dcac548b9
Tamal Saha <tamal@appscode.com>
2021-10-22 01:35:28 -0700

    added-all

---
+++ getters/providers/issuer.go
@@ -0,0 +1,34 @@
+package providers
+
+import (
+	"crypto/x509"
+	"fmt"
+	"github.com/tamalsaha/java-cacert-demo/getters/lib"
+	"kmodules.xyz/client-go/tools/configreader"
+	"gomodules.xyz/cert"
+)
+
+type CAGetterIssuer struct {
+	r configreader.ConfigReader
+	key lib.ObjectKey
+}
+
+var _ lib.CAGetter = &CAGetterIssuer{}
+
+func (c *CAGetterIssuer) Init() error {
+	_, err := c.r.Issuers(c.key.Namespace).Get(c.key.Name)
+	return err
+}
+
+func (c *CAGetterIssuer) GetCAs(key string) ([]*x509.Certificate, error) {
+	secret, err := c.r.Issuers(c.key.Namespace).Get(c.key.Name)
+	if err != nil {
+		return nil, err
+	}
+	data, ok := secret.Data[key]
+	if !ok {
+		return nil, fmt.Errorf("missing key %s in secret %s/%s", key, c.key.Namespace, c.key.Name)
+	}
+	caCerts, _, err := cert.ParseRootCAs(data)
+	return caCerts, err
+}
